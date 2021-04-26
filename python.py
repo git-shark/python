fedc881b4759c0839b965249157ecd93d486fcc9
git-shark <dj.shark.01@rakuten.jp>
2021-04-26 17:51:09 +0900

    学習1 2021.04.26

--- python.py
+++ python.py
@@ -2,21 +2,16 @@
 num_1 = input("num_1 =>")
 num_2 = input("num_2 =>")
 ans = int(num_1) + int(num_2)
-print("num_1("+str(num_1)+")+num_2("+str(num_2)+")="+str(ans))
 
 #完成イメージから分解して考えて書くと良い
-#num_1(10)+num_2(20)=30
+#(完成形)num_1(10)+num_2(20)=30
+#num_1(      文字部分
+#10          代入部分
+#)+num_2(    文字部分
+#20          代入部分
+#)=          文字部分
+#30          代入部分
 
-print(
-"num_1("
-+str(num_1)+
-")+num_2("
-+str(num_2)+
-")="
-+str(ans)
-)
+print("num_1("+str(num_1)+")+num_2("+str(num_2)+")="+str(ans))
 
-msg_ans = "num_1({num_1})+num_2({num_2})={ans}".format(num_1=num_1,num_2=num_2,ans=ans)
-print(msg_ans)
-msg_ans = "num_1({key0})+num_2({key1})={key2}".format(key0=num_1,key1=num_2,key2=ans)
-print(msg_ans)
+msg_ans = "num_1({index0})+num_2({index1})={index2}".format(index0=num_1,index1=num_2,index2=ans)
