2529e8a36b77fadf0adcb9029869f7a751f6fb43
git-shark <dj.shark.01@rakuten.jp>
2021-05-06 21:45:51 +0900

    no3

--- 2021.05.06.py
+++ 2021.05.06.py
@@ -2,26 +2,11 @@
 #listやtupleに値を代入する際の注意
 #文字扱いの場合はクオーテーションで囲む必要あり
 
-list = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    "f"]
+list = [1, 2]
 
-tuple = (
-    "a",
-    "b",
-    "c"
-)
+tuple = ("a","b")
 
-dict = {
-    "key":10
-    "キー":20
-    "きー":30
-    }
+dict = {"key":10,"キー":20,"きー":30}
 
 for list_data in list:
     print(list_data)
@@ -29,5 +14,15 @@ for list_data in list:
 for tuple_data in tuple:
     print(tuple_data)
 
-for dict_key in dict:
-    print(dict_key)
+for key in dict:
+    value = dict[key]
+    print("{key}:{value}".format(key=key,value=value))
+
+for k,v in dict.items():
+    print("{0}:{1}".format(k,v))
+
+for key in dict.keys():
+    print("key={0}".format(key))
+
+for value in dict.values():
+    print("value={0}".format(value))
