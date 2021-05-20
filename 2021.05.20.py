8bdfe0522eac2b875a01d11c25f14b62ea29e74d
git-shark <dj.shark.01@rakuten.jp>
2021-05-21 00:35:23 +0900

    commit01

--- 2021.05.20.py
+++ 2021.05.20.py
@@ -24,3 +24,13 @@ print(sentence_list)
 
 sentence_list = sentence.splitlines(True)
 print(sentence_list)
+
+print(color.replace("ed", "oooo"))
+
+key_word = "あかさたなはまやらわをん"
+trans_table = str.maketrans("あか","おお","わをん")   #第一,第二引数に文字数差があるとエラー。※第三引数は文字数に関係しない
+print(key_word.translate(trans_table))
+
+print(key_word.strip("あさなまらを"))     #必ず左端から順にデータを削除する、削除できない部分が残ると終了
+print(key_word.lstrip("あさ"))           #必ず左端から順にデータを削除する、削除できない部分が残ると終了
+print(key_word.rstrip("んわ"))           #必ず右端から順にデータを削除する、削除できない部分が残ると終了
