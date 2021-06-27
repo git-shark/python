fa9c757865743d5004b72b01ee9ddd8e8b15a9ae
git-shark <dj.shark.01@rakuten.jp>
2021-06-27 23:03:25 +0900

    commit

--- テトリス.py
+++ テトリス.py
@@ -21,33 +21,74 @@ def DrawBlock(copy_board, block_coords, x, y, block_shape):
     global curBlockCoords, curShape, curX, curY
 
     SetBlockOnBoard(copy_board, block_coords, x, y, block_shape)
-    board = copy.deepcopy(copy_board)  #要素(値)だけ代入するのではなくオブジェクト(インスタンス)ごと代入している
+    board = copy.deepcopy(copy_board)
     curBlockCoords = copy.deepcopy(block_coords)
     [curShape, curX, curY] = [block_shape, x, y]
-    panel.Refresh()
+    panel.Refresh()                    #前データを削除し再描画  ※再描写後「描写メッセージ(EVT_PAINT)が発生」
 
-def OnTimer(event):
-    global isCurBlock
+def CheckMoveAvailable(check_board, block_coords, x, y):
+    for i in range(4):
+        piece_x = block_coords[i][0] + x
+        piece_y = block_coords[i][1] + y
+        if piece_x < 0 or piece_x > BoardWidth-1 or \
+            piece_y < 0 or piece_y > BoardHeight-1 \
+                or check_board[piece_y][piece_x] != 0:
+            return False
+        return True
 
-    if event.GetId() != ID_TIMER:
+def OnKeyDown(event):
+    global isCurBlock, isKeyDown
+
+    if keyAllowed is False or isKeyDown is True:
         return
 
+    isKeyDown = True
+
+    keycode = event.GetKeyCode()
+    if keycode == wx.WXK_LEFT or keycode == wx.WXK_RIGHT:
+        copy_board = copy.deepcopy(board)
+        setBlockCoords(copy_board, curBlockCoords, curX, curY, 0)
+        nextBlockCoords = copy.deepcopy(curBlockCoords)
+        [nextX, nextY] = [curX, curY]
+
+    if keycode == wx.WXK_LEFT:
+        nextX = curX -1
+    elif keycode == wx.WXK_RIGHT:
+        nextX = curX + 1
+
+    if checkMoveAvailable(copy_board, next_BlockCoords, nextX, nextY):
+        DrawBlock(copy_board, nextBlockCoords, nextX, nextY, curShape)
+        isCurBlock = True
+    isKeyDown = False
+
+def OnTimer(event):                    #event=panel、慣例で仮引数には(event)表記を用いる
+    global isCurBlock, keyAllowed      #フラグ(描写判定用)グローバル化
+
+    if event.GetId() != ID_TIMER or isKeyDown is True:      #イベント発生源がpanelか否かの判定
+        return
+
+    keyAllowed = False
     copy_board = copy.deepcopy(board)
     if not isCurBlock:
         newShape = random.randint(1, 7)
         newBlockCoords = copy.deepcopy(BlockTable[newShape])
         [newX, newY] = [int(BoardWidth / 2), 1]
-        DrawBlock(copy_board, newBlockCoords, newX, newY, newShape)
-        isCurBlock = True
-    else:
-        SetBlockOnBoard(copy_board, curBlockCoords, curX, curY, 0)
-        if curY > BoardHeight - 3:
+        if not CheckMoveAvailable(copy_board, newBlockCoords, newX, newY):
             event.GetTimer().Stop()
             return
-        DrawBlock(copy_board, curBlockCoords, curX, curY + 1, curShape)
+        else:
+            DrawBlock(copy_board, newBlockCoords, newX, newY, newShape)
+            isCurBlock = True
+    else:
+        SetBlockOnBoard(copy_board, curBlockCoords, curX, curY, 0)
+        if not CheckMoveAvailable(copy_board, curBlockCoords, curX, curY+1):
+            isCurBlock = False
+        else:
+            DrawBlock(copy_board, curBlockCoords, curX, curY+1, curShape)
+    keyAllowed = True
 
 def OnPaint(event):
-    obj = event.GetEventObject()
+    obj = event.GetEventObject()               #obj = <wx._core.Panel object at 0x0000022D73074AF0> がBoardHeight分処理
     dc = wx.BufferedPaintDC(obj)
     dc.Clear()
 
@@ -83,14 +124,13 @@ def Main():
     global colors, light, dark
     global board
     global BlockTable
-    global isCurBlock
+    global isCurBlock, keyAllowed, isKeyDown
     global panel
     global ID_TIMER, timer
 
     [PieceSize, BoardWidth, BoardHeight] = [20, 10, 22]
     PanelWidth = PieceSize * BoardWidth
     PanelHeight = PieceSize * BoardHeight
-
     ShapeNo = [[0, 0], [0, 0], [0, 0], [0, 0]]
     ShapeZ = [[0, -1], [0, 0], [-1, 0], [-1, 1]]
     ShapeS = [[0, -1], [0, 0], [1, 0], [1, 1]]
@@ -106,26 +146,27 @@ def Main():
     light = ['BLACK']
     dark = ['BLACK']
     for i in range(1, 8):
-        light.append(colors[i].ChangeLightness(150))
+        light.append(colors[i].ChangeLightness(150)) #['BLACK', wx.Colour(255, 127, 127, 255), wx.Colo・・・・
         dark.append(colors[i].ChangeLightness(50))
 
     board = []
     for i in range(BoardHeight):
         board_row = [0] * BoardWidth
         board.append(board_row)
-    isCurBlock = False
+    [isCurBlock, keyAllowed, isKeyDown] = [False] * 3
 
     frame = wx.Frame(None, title = 'TetGame')
     frame.SetClientSize(PanelWidth, PanelHeight)
     panel = wx.Panel(frame)
     panel.SetBackgroundColour('BLACK')
-    panel.Bind(wx.EVT_PAINT, OnPaint)
+    panel.Bind(wx.EVT_PAINT, OnPaint)                 #panel.Refresh()実行 => (EVT_PAINT)が発生 => OnPaint関数へ
     panel.Bind(wx.EVT_ERASE_BACKGROUND, OnEraseBackground)
     ID_TIMER = 1
-    timer = wx.Timer(panel, ID_TIMER)
-    panel.Bind(wx.EVT_TIMER, OnTimer)
-    Speed = 100  #ミリ秒
-    timer.Start(Speed)
+    timer = wx.Timer(panel, ID_TIMER)                 #panelのIDを1としている
+    panel.Bind(wx.EVT_TIMER, OnTimer, id = ID_TIMER)                 #タイマーイベント発生毎に、コールバック関数OnTimeを実行
+    Speed = 100                                       #ミリ秒
+    timer.Start(Speed)                                #100ms毎にイベント(EVT_TIMER)発生
+    panel.Bind(wx.EVT_CHAR_HOOK, OnKeyDown)
     frame.Center()
     frame.Show()
 
